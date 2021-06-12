02474114b6595de0e727d23926f6d18b2a57986c
Nicholas Lee <csudcy@gmail.com>
2021-06-12 08:30:57 +0100

    Add deps:add command

---
+++ scripts/deps_add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Add dependencies for this service.
+
+# Change to the parent directory
+cd "${BASH_SOURCE%/*}"
+cd ..
+
+echo "Upgrading pip..."
+poetry run pip install --upgrade pip
+
+echo "Adding dependencies..."
+poetry add $@
+
+echo "Generating requirements.txt file..."
+poetry export --format=requirements.txt --without-hashes > src/requirements.txt
